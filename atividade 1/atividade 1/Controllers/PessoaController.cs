280aba8accdec12a90955025d6cecec77d1e5cc4
Ana Laura Andrade <analaura140600@gmail.com>
2024-08-18 19:26:03 -0300

    .

---
+++ atividade 1/atividade 1/Controllers/PessoaController.cs
@@ -0,0 +1,16 @@
+﻿using atividade_1.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace atividade_1.Controllers
+{
+    [ApiController]
+    [Route("api/pessoa")]
+    public class PessoaController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult CalcularImc(Pessoa pessoa)
+        {
+           return Ok($"O valor do IMC da(o) {pessoa.Nome} é  {pessoa.Imc().ToString("F2")}");
+        }
+    }
+}
---
+++ atividade 1/atividade 1/Models/Pessoa.cs
@@ -0,0 +1,16 @@
+﻿namespace atividade_1.Models
+{
+    public class Pessoa
+    {
+        public string Nome { get; set; }
+        public double Peso { get; set; }
+        public double Altura { get; set; }
+
+
+        public double Imc()
+        {
+            return Peso / (Altura * Altura);
+        }
+    }
+
+}

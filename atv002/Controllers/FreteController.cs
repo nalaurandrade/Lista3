7906facc93e1fd53ff3e4c88b817678e1b931e80
Ana Laura Andrade <analaura140600@gmail.com>
2024-08-18 18:50:21 -0300

    Add project files.

---
+++ atv002/Controllers/FreteController.cs
@@ -0,0 +1,16 @@
+﻿using atv002.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace atv002.Controllers
+{
+    [ApiController]
+    [Route("api/frete")]
+    public class FreteController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult CalcularFrete(Frete frete)
+        {
+            return Ok($"O valor do frete com volume {frete.Produto} é {frete.Volume().ToString("F2")}");
+        }
+    }
+}
---
+++ atv002/Models/Frete.cs
@@ -0,0 +1,19 @@
+﻿namespace atv002.Models
+{
+    public class Frete
+    {
+        public string Produto { get; set; }
+        public float PesoProduto { get; set; }
+        public float AlturaProduto { get; set; }
+        public float LarguraProduto { get; set; }
+        public float ComprimentoProduto { get; set; }
+        public string estado { get; set; }
+
+        public float GetLarguraProduto()
+        {
+            return LarguraProduto;
+        }
+
+        public double Volume; (float larguraProduto) AlturaProduto* LarguraProduto;
+    }
+}
